b35d40a08abef99654cdb10b390b9266dc01e633
tomas <tmaldonadotrs@gmail.com>
2023-02-01 20:41:01 -0800

    Working on contact form submission.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+$("#about_container").scrollTop(0);
+$("#work_container").scrollTop(0);
+$("#contact_container").scrollTop(0);
+
 function showabout(){
     $("#about_container").css("display","inherit");
     $("#about_container").addClass("animated slideInLeft");
@@ -27,6 +31,7 @@ function closework(){
     },800);
 }
 function showcontact(){
+    $('#contact_container').scrollTop(0);
     $("#contact_container").css("display","inherit");
     $("#contact_container").addClass("animated slideInUp");
     setTimeout(function(){
@@ -66,3 +71,27 @@ $('#toggle-audio').on('click', function () {
     }
     
 })
+/*
+$('#submit-msg').on('click', function(){
+    $.ajax({
+        url: 'contactScript.php',
+        type: 'POST',
+        data: {
+            submit: 'submit',
+            name: $('input[name="name"]').text(),
+            email: $('input[name="email"]').text(),
+            message: $('input[name="message"]').text(),
+        },
+        success: function(){
+            $('input[name="name"]').val('');
+            $('input[name="email"]').val('');
+            $('input[name="message"]').val('');
+            $('.sent').addClass('sent-visible');
+            console.log('ajax success');
+            setTimeout(() => {
+                $('.sent').removeClass('sent-visible');
+            }, 8000);
+            
+        }
+    })
+})*/
